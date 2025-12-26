4176a8d78ae24411fdb4f0b8172ef19b16e22407
Mitzury <bogdanovsi@gmail.com>
2025-12-26 19:32:22 +0300

    course

--- src/main/java/ru/mitzury/course/HelloServlet.java
+++ src/main/java/ru/mitzury/course/HelloServlet.java
@@ -4,6 +4,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
@@ -14,5 +19,11 @@ public class HelloServlet extends HttpServlet {
 
         resp.setContentType("text/plain; charset=UTF-8");
         resp.getWriter().println("Hello from Embedded Tomcat 11!");
+
+
+            try (Document document = new Document(new PdfDocument(new PdfWriter("./hello-pdf.pdf")))) {
+                document.add(new Paragraph("Hello PDF!"));
+            }
+
     }
 }
